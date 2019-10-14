8f70356d9b2e7add397645768a9c873bc0061db9
krikaz <pascal.bou@protonmail.com>
2019-10-14 16:37:43 +0200

    dequeue not working

--- queue_and_stack/dll_queue.py
+++ queue_and_stack/dll_queue.py
@@ -1,19 +1,23 @@
-import sys
-sys.path.append('../doubly_linked_list')
+# import sys
+# sys.path.append('../doubly_linked_list')
+# print(sys.path)
+
 from doubly_linked_list import DoublyLinkedList
 
-
-class Queue:
+class Queue():
     def __init__(self):
         self.size = 0
         # Why is our DLL a good choice to store our elements?
         # self.storage = ?
+        self.storage = DoublyLinkedList()
 
     def enqueue(self, value):
-        pass
+        print(self.storage.__len__())
+        self.storage.add_to_tail(value)
+        print(self.storage.__len__())
 
     def dequeue(self):
-        pass
+        return self.storage.remove_from_head()
 
     def len(self):
-        pass
+        return self.storage.__len__()
